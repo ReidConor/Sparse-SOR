e9672ce378004779ff69a8d3e5015d46d533548f
Conor Reid <coreid@tcd.ie>
2016-10-29 12:11:17 +0100

    Initial commit

---
+++ core/main.py
@@ -0,0 +1,5 @@
+# This will function as the main module of te python project
+# and call all other modules that are written
+
+def add(x,y):
+    return x + y
---
+++ tests/testMain.py
@@ -0,0 +1,14 @@
+# This will hold all tests for the main program
+# All future tests should follow the same namig convention (test<core_file>)
+import unittest
+from core.main import add
+
+class MyTest(unittest.TestCase):
+    def test_true(self):
+        self.assertEqual(add(2,2), 4)
+
+class AnotherTest(unittest.TestCase):
+    def test_another(self):
+        self.assertTrue(add(2,2) == 4)
+
+unittest.main()
