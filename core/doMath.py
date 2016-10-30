7f99214b7e9c7fc61283545c0190662093ea00b4
Conor Reid <coreid@tcd.ie>
2016-10-30 11:06:22 +0000

    changes to hasZeros method

--- core/doMath.py
+++ core/doMath.py
@@ -1,12 +1,7 @@
 import numpy as np
 
 def hasZerosOnMainDiag (A):
-    hasZero = False
-    diag = np.diagonal(A)
-    if np.in1d(0, diag):
-        hasZero = True
-    return hasZero
-
+    return np.in1d(0, np.diagonal(A))
 
 # this is just to check out how to do tests in python
 def add (x,y):
