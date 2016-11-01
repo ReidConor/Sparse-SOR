353834de84c12644b7349b07b854a74f55fbe102
Conor Reid <coreid@tcd.ie>
2016-11-01 07:22:32 +0000

    Work in progress

--- core/doMath.py
+++ core/doMath.py
@@ -1,7 +1,65 @@
 import numpy as np
-
+import sys
 def hasZerosOnMainDiag (A):
     return np.in1d(0, np.diagonal(A))
+
+'''
+findX_noSOR finds solution for x, ie not using SOR
+that is, follows the below logic...
+Ax = b
+(A^-1)(A)(x) = (A^-1)(b)
+(1)(x) = (A^-1)(b)
+so...x = the dot product of inverse(A) times b
+
+this can be used to check the results of SOR against
+'''
+def findX_direct(A, b):
+    A = A.todense()
+    x = np.dot(np.linalg.inv(A), b)
+    print ("Solution of x (no sor) = ", x)
+
+
+'''
+omega must be > 1 to be the successive over-relaxation method.
+< 1 its the successive under-relaxation
+'''
+def Sparse_SOR (A,b,n,maxits,omega,x, error):
+    k = 0
+    while k < maxits:
+        # print ("Iteration:" ,k)
+
+        omega_prev_prev = omega + 0.1
+        omega_prev = omega + 0.05
+        rows = b.shape[0]
+        D = A.diagonal()
+        err = find_residual(A, x, b)
+        err_prev = err + 1.0
+        while err > error:
+            s = np.zeros(b.shape[0])
+            for row in range(rows):
+                cols = A.indices[A.indptr[row]:A.indptr[row + 1]]
+                for j, col in enumerate(cols):
+                    if col != row:
+                        s[row] += A.data[A.indptr[row]:A.indptr[row + 1]][j] * x[col]
+
+                if D[row] != 0.0:
+                    x[row] += omega * ((b[row] - s[row]) / D[row] - x[row])
+            k += 1
+            err_prev = err
+            err = find_residual(A, x, b)
+        print("SOR: x=", x)
+
+
+
+'''
+From the notes:
+There are two common ways to measure the discrepancy between the true solution x and the computed solution xˆ:
+    Error: δx=x−xˆ
+    Residual: r = b − Axˆ
+'''
+def find_residual (A,x,b):
+    return np.linalg.norm(b - A.dot(x))
+
 
 # this is just to check out how to do tests in python
 def add (x,y):
--- core/main.py
+++ core/main.py
@@ -1,14 +1,28 @@
-# This will function as the main module of te python project
-# and call all other modules that are written
+
 from core.fileIO import *
 from core.doMath import *
 
+'''
+This will function as the main module of te python project
+and call all other modules that are written
+'''
+
+
 # get the problem space from txt file, ie. A and b from "Ax = b"
 theProblemSpace = readFileIntoDict()
+A = theProblemSpace["A"]
+b = theProblemSpace["b"]
 
-#  check for zeros on the main diag (one of the stopping rules)
-# use todense() to give the nornal matrix representation of A
-if hasZerosOnMainDiag(theProblemSpace["A"].todense()):
+'''
+check for zeros on the main diag (one of the stopping rules)
+use todense() to give the nornal matrix representation of A
+'''
+if hasZerosOnMainDiag(A.todense()):
     print ("Cant go on")
 else:
-    print ("Continue")
+    print ("Can continue..")
+    findX_direct(A, b)
+
+    # passing dummy values until I end up using them correctly in the function
+    # if its zero, its not used
+    Sparse_SOR (A,b,0,1,1.5,np.zeros(b.shape[0]),0.00000001)
