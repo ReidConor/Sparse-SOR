73085d0019683a6c84df34d464db8aa7ffef07be
Conor Reid <conor.reid@ucdconnect.ie>
2016-11-13 13:30:39 +0000

    adding in ability to pass input and output files as args to program

--- core/fileIO.py
+++ core/fileIO.py
@@ -2,15 +2,15 @@ import numpy as np
 from scipy import sparse
 
 # read text file into A and b
-def readFileIntoDict():
-    lines = open('input.in','r').readlines()
+def readFileIntoDict(inputFile):
+    lines = open(inputFile, 'r').readlines()
     myDict = {}
 
     A = []
     dim = int(lines[0])
     b = [lines[dim+1]]
-    for x in range (1, dim+1):
-        A.append (lines[x])
+    for x in range(1, dim+1):
+        A.append(lines[x])
 
     # want to use Compressed Sparse Row Format (csr)
     myDict["A"] = sparse.csr_matrix(np.loadtxt(A))
