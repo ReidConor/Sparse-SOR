741ed3f213b89824e42e61addb176e7ffd5e5a91
louiscarnec <Carnec@Louiss-MacBook-Pro.local>
2016-11-13 22:04:49 +0000

    asking user for path

--- core/SOR.py
+++ core/SOR.py
@@ -13,6 +13,57 @@ from scipy.io import mminfo,mmread
 from scipy.sparse import csr_matrix
 import numpy as np
 import numpy.random
+import sys
+
+''' 
+Checking if zeros on the Diagonal
+'''
+
+
+def hasZerosOnMainDiag (A):
+    return np.in1d(0, np.diagonal(A))
+ 
+'''
+Checking if matrix is strictly diagonally dominant
+'''
+    
+def isStrictlyDiagDom(A):
+    rowcol = np.shape(A)[1]
+    i = 0
+    j = 0
+    sumrow = 0 
+    count = 0
+    for i in range(n):
+        sumrow = 0
+        for j in range(n):
+            if i != j:
+                sumrow = sumrow + abs(A[i,j])
+        if abs(A[i,i]) > sumrow:
+            count += 1
+    return count
+
+'''
+findX_noSOR finds solution for x, ie not using SOR
+that is, follows the below logic...
+Ax = b
+(A^-1)(A)(x) = (A^-1)(b)
+(1)(x) = (A^-1)(b)
+so...x = the dot product of inverse(A) times b
+
+this can be used to check the results of SOR against
+'''
+def findX_direct(A, b):
+    A = A.todense()
+    x = np.dot(np.linalg.inv(A), b)
+    print ("Solution of x (no sor) = ", x)
+
+def find_residual (A,x,b):
+    return np.linalg.norm(b - A.dot(x))
+
+
+# this is just to check out how to do tests in python
+def add (x,y):
+    return x+y
 # IMPORTING MATRIX FROM market matrix
  
  
@@ -20,7 +71,7 @@ import numpy.random
 #m = mmread("/Users/Carnec/Desktop/Business_Analytics/NumAnYr2/assignment2/plat1919.mtx")
 #print(m)
 
-m = m.asformat("csr") #transform from COO to CSR format
+#m = m.asformat("csr") #transform from COO to CSR format
 
 # USER INPUT MATRIX
 
@@ -46,12 +97,14 @@ val = m.data #values
 rowstart = m.indices #rowstart
 col = m.indptr #col
 
-def Sparse_SOR(A,b,maxits,epsilon,w):
+def Sparse_SOR(A,b,n,maxits,epsilon,omega):
     n = A.get_shape()[1]
     k = 0
     x = np.random.rand(n)
     diff = 5555555.0
+    xnorm=0
     while k <= maxits and diff > epsilon:
+        xnorm0 = xnorm
         for i in range(0,n):
             thesum = 0
             for j in range(int(A.indptr[i]),int(A.indptr[i+1])):
@@ -59,10 +112,12 @@ def Sparse_SOR(A,b,maxits,epsilon,w):
                 if col[j] == i:
                     d=val[j]
             x0 = x[i]
-            x[i] = x[i] + w *(b[i]-thesum)/d
-            diff = abs(x[i] - x0)
-            print(diff)
+            x[i] = x[i] + omega *(b[i]-thesum)/d
+        xnorm = np.linalg.norm(x, ord=2)
+        print('xnorm',xnorm)
+        diff = xnorm - xnorm0
         k = k+1
+        print(diff)
     return x
         
 #    
@@ -77,4 +132,4 @@ x = np.ones(n)
 b = np.random.randint(0,5,size=3)
 
 
-print(Sparse_SOR(matrix_example,b,10,0.005,0.5))
+print(Sparse_SOR(matrix_example,b,n,100,0.00005,1.5))
--- core/fileIO.py
+++ core/fileIO.py
@@ -1,9 +1,31 @@
 import numpy as np
 from scipy import sparse
+from scipy.io import mminfo,mmread
 
-# read text file into A and b
-def readFileIntoDict(inputFile):
-    lines = open(inputFile, 'r').readlines()
+#ask for path of file
+
+'''
+Ask user to input matrix
+'''
+
+def inputMatrix():
+    f = input("\n Hello, user. "
+        "\n \n Please type in mtx if you are using an mtx file or txt if you are using a text file and press 'Enter': ")
+    if f=='mtx':
+        inputmtx = input("\n \n Please type in the path of the mtx file and press 'Enter': ")
+        m =  mmread(inputmtx)
+    elif f == 'txt':
+        inputtxt = input("\n \n Please type in the path of the txt file and press 'Enter': ")
+        m =  inputtxt 
+    return f, m
+    
+
+'''
+Read matrix A and b into a dictionary
+'''
+
+def readFileIntoDict(m):
+    lines = open(m, 'r').readlines()
     myDict = {}
 
     A = []
@@ -16,3 +38,17 @@ def readFileIntoDict(inputFile):
     myDict["A"] = sparse.csr_matrix(np.loadtxt(A))
     myDict["b"] = np.loadtxt(b)
     return myDict
+    
+    
+def readFileMtx(m):
+    myDict = {}
+
+    
+    A = m.asformat("csr")
+    dim = m.shape[1]
+    b = np.random.randint(0,10,size=dim)
+    
+    myDict["A"] = A
+    myDict["b"] = b
+    return myDict
+    
